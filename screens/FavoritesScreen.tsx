a86951934a44babeb6b7305e881d18246d7d092c
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2025-08-02 18:38:59 -0500

    adds favorites empty state

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,8 +1,22 @@
 import { useMemo } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 import { MealsList } from '../components/MealsList/MealsList';
 import { useFavorites } from '../hooks/useFavorites';
 import { MEALS } from '../data/dummy-data';
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
 export const FavoritesScreen = () => {
   const { ids: favoriteIds } = useFavorites();
@@ -10,6 +24,14 @@ export const FavoritesScreen = () => {
   const meals = useMemo(() => {
     return MEALS.filter(({ id }) => favoriteIds.includes(id));
   }, [favoriteIds]);
+
+  if (!meals.length) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
 
   return <MealsList meals={meals} />;
 };
